53da06201be6b5c46fc7eba94c7d62066f5c32af
Felipe Fausto <fellipe.fausto@hotmail.com>
2021-09-07 15:28:40 -0300

    my first react mobile app

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet, Button, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Meu primeiro Mobile APP</Text>
+      <Button title="Botão Mobile APP" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
